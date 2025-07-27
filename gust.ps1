8f58eae045a7edd8a28650e6a1c80ea79f9130c0
Treecher <treechcer@gmail.com>
2025-07-27 23:34:28 +0200

    added more things this script can do - yes most of them really easily done in normal git but I want to make more things with them, for now I just made them as possible things

--- gust.ps1
+++ gust.ps1
@@ -3,41 +3,86 @@ param(
     [string]$message,   # commit message
 
     [Alias("u")]
-    [string]$gitURL     # URL, not necessary if you have alread "git add ."
+    [string]$gitURL,     # URL, not necessary if you have alread "git add ."
+
+    [Alias("m")]
+    [string]$otherModes, # this adds more possibilities / "modes"
+
+    [Alias("b")]
+    [string]$branch # this is the name of the branch you'll use if you do branch create switch (bcs) mode
 )
 
-$username = git config --global user.name
-$useremail = git config --global user.email
+function checkUser{
+    $username = git config --global user.name
+    $useremail = git config --global user.email
 
-if (-not $username) {
-    $username = git config user.name
-}
-if (-not $useremail) {
-    $useremail = git config user.email
-}
+    if (-not $username) {
+        $username = git config user.name
+    }
+    if (-not $useremail) {
+        $useremail = git config user.email
+    }
 
-if (-not $username -or -not $useremail) {
-    Write-Host "--------------------------------------------------------------------------------------------------"
-    Write-Host "Please set your username and/or email with:"
-    Write-Host "git config --global user.name 'your name'"
-    Write-Host "git config --global user.email 'your email'"
-    Write-Host ""
-    Write-Host "If you want to set local email and username, just skip '--global' and you'll set them for your repository"
-    Write-Host "--------------------------------------------------------------------------------------------------"
-    exit 1
-}
-
-if ($gitURL){
-    git init 1>$null 3>$null 4>$null 5>$null 6>$null
-    git remote add origin "$gitURL.git" 1>$null 3>$null 4>$null 5>$null 6>$null
-    git pull origin main --allow-unrelated-histories 1>$null 3>$null 4>$null 5>$null 6>$null
-    git push --set-upstream origin main 1>$null 3>$null 4>$null 5>$null 6>$null
+    if (-not $username -or -not $useremail) {
+        Write-Host "--------------------------------------------------------------------------------------------------"
+        Write-Host "Please set your username and/or email with:"
+        Write-Host "git config --global user.name 'your name'"
+        Write-Host "git config --global user.email 'your email'"
+        Write-Host ""
+        Write-Host "If you want to set local email and username, just skip '--global' and you'll set them for your repository"
+        Write-Host "--------------------------------------------------------------------------------------------------"
+        exit 1
+    }
 }
 
-$errAdd = git add . 1>$null 3>$null 4>$null 5>$null 6>$null
+function gitPushCreate{
+    if ($gitURL){
+        git init 1>$null 3>$null 4>$null 5>$null 6>$null
+        git remote add origin "$gitURL.git" 1>$null 3>$null 4>$null 5>$null 6>$null
+        git pull origin main --allow-unrelated-histories 1>$null 3>$null 4>$null 5>$null 6>$null
+        git push --set-upstream origin main 1>$null 3>$null 4>$null 5>$null 6>$null
+    }
 
-$errCom = git commit -m "$message" 1>$null 3>$null 4>$null 5>$null 6>$null
+    $errAdd = git add . 1>$null 3>$null 4>$null 5>$null 6>$null
 
-git push 1>$null 3>$null 4>$null 5>$null 6>$null
+    $errCom = git commit -m "$message" 1>$null 3>$null 4>$null 5>$null 6>$null
 
-Write-Host ""
+    git push 1>$null 3>$null 4>$null 5>$null 6>$null
+}
+
+function branchCreateSwitch{
+    git branch
+    git branch $branch
+    git checkout $branch
+}
+
+function branchSwitch{
+    git checkout $branch
+    git branch
+}
+
+function branchDelte{
+    git branch -D $branch
+    git branch
+}
+
+function behaviourCheck{
+    $otherModes
+    switch ($otherModes){
+        "" { # just git add, commit and push :)
+            gitPushCreate
+        }
+        "bcs" { # branch create switch
+            branchCreateSwitch
+        }
+        "bs" { # branch switch
+            branchSwitch
+        }
+        "bd" { # branch delte
+            branchDelte
+        }
+    }
+}
+
+checkUser
+behaviourCheck
