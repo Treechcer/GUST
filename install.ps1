dd5f92c2251548d4054f26bf20b2219397a0f171
Treecher <treechcer@gmail.com>
2025-10-16 19:10:49 +0200

    added automatic download + Path adder, added a way to update automatically too, with gust --update, ver 0.3.5

--- install.ps1
+++ install.ps1
@@ -1,4 +1,26 @@
+param(
+    [Boolean]$install = $false
+)
+
 function install{
     mkdir temp
     git clone https://github.com/Treechcer/GUST temp
+}
+
+function installAuto {
+    install
+    robocopy $PSScriptRoot\temp $PSScriptRoot /E /IS /IT
+    Remove-Item -Path $PSScriptRoot/temp -Recurse -Force
+    $currentPath = [Environment]::GetEnvironmentVariable("Path", "User")
+    if ($currentPath -notlike "*$PSScriptRoot*") {
+        [Environment]::SetEnvironmentVariable("Path", "$currentPath;$PSScriptRoot", "User")
+        Write-Host "Gust, from folder $PSScriptRoot was added do PATH. Restart your terminal."
+    }
+    else{
+        Write-Host "Already in path"
+    }
+}
+
+if ($install){
+    installAuto
 }
